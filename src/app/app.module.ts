a82c0ab99fedebab1663d40180372a07c2275712
Andrew B. Kim <andrew.kim.english@gmail.com>
2020-11-27 06:27:33 +0900

    Initial Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
---
+++ src/app/material.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+const modules = [MatButtonModule, MatToolbarModule, MatIconModule];
+@NgModule({
+  declarations: [],
+  imports: modules,
+  exports: modules,
+  // CommonModule
+})
+export class MaterialModule { }
